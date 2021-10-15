c79f97b94d962d66e4e486021833cc150582f7b8
AGPA-88 <agpa88@gmail.com>
2021-10-16 00:04:37 +0200

    Added footer

--- FrontEnd/src/components/MyFooter.vue
+++ FrontEnd/src/components/MyFooter.vue
@@ -1,7 +1,22 @@
 <template>
     <footer>
-        here is my footer
-    </footer>
+        <div class="footy-sec mn no-margin">
+            <div class="container">
+                <ul>
+                    <li><a href="#" title="">Help Center</a></li>
+                    <li><a href="#" title="">Privacy Policy</a></li>
+                    <li><a href="#" title="">Community Guidelines</a></li>
+                    <li><a href="#" title="">Cookies Policy</a></li>
+                    <li><a href="#" title="">Career</a></li>
+                    <li><a href="#" title="">Forum</a></li>
+                    <li><a href="#" title="">Language</a></li>
+                    <li><a href="#" title="">Copyright Policy</a></li>
+                </ul>
+                <p><img src="images/copy-icon2.png" alt="">Copyright 2021</p>
+                <img class="fl-rgt" src="images/icon-left-font-monochrome-black-small.png" alt="">
+            </div>
+        </div>
+    </footer><!--footer end-->
 </template>
 <script>
 export default {
@@ -9,9 +24,6 @@ export default {
 };
 </script>
 <style>
-    footer{
-        height: 200px;
-        width: 100%;
-        background-color: lightgrey;
-    }
+    @import url('../styles/footer.css');
+    @import url('../styles/common.css');
 </style> 
