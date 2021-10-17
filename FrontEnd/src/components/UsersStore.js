791b09a5f751e938c0dc4e19c75c7d124b35a462
j0rdan-m <jordan.mariani>
2021-10-17 21:36:39 +0200

    Add mutations & getters on the store

--- FrontEnd/src/components/UsersStore.js
+++ FrontEnd/src/components/UsersStore.js
@@ -3,9 +3,34 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    getters: {},
-    actions: {}
+const state = {
+    user: {
+        firstname: "Antonio",
+        lastname: "Alexandre",
+        job: "Front End developer",
+        following: 34,
+        followers: 155
+    }
+};
+
+const mutations = {
+    ADD_FOLLOWING: (state) => {
+        state.user.following++;
+    }
+};
+
+const getters = {
+    user_firstname: state => state.user.firstname,
+    user_lastname: state => state.user.lastname,
+    user_job: state => state.user.job
+};
+
+let store =  new Vuex.Store({
+    state: state,
+    mutations: mutations,
+    getters: getters,
+    actions: {},
+    strict: true
 });
+
+export default store;
