4f41441b01633186dcc82ba14ceb3a295f730778
AGPA-88 <agpa88@gmail.com>
2021-10-23 00:35:59 +0200

    Post title and description added to main post card shadow added to cards and header

--- FrontEnd/src/components/GmnPost.vue
+++ FrontEnd/src/components/GmnPost.vue
@@ -14,14 +14,20 @@
                     <button><i class="material-icons-outlined">more_horiz</i></button>
                 </div>
             </div>
-            <div class="whitespace-pre-wrap mt-7 text-style">{{ post.title }}</div>
+            <div class="whitespace-pre-wrap mt-7 text-style">
+                <h1>{{ post.title }}</h1>
+
+            </div>
             <div class="mt-5 flex gap-2	 justify-center border-b pb-4 flex-wrap	">
                 <img :src="post.imageUrl" class="bg-red-500 rounded-2xl w-1/3 object-cover h-96 flex-auto" alt="photo">
                 
             </div >
+            <div class="whitespace-pre-wrap mt-7 text-style">
+                <p>{{ post.description }}</p>
+            </div>
             <div class="interactions rounded-2xl">
                 <div class=" h-16 border-b  flex items-center justify-around">
-                    <div class="flex items-center	gap-3	">
+                    <div class="flex items-center	gap-3	icon-btn">
                         <span class="material-icons-outlined icon-style">
                             chat_bubble_outline
                         </span>
@@ -29,7 +35,7 @@
                     </div>
                     <div class="flex items-center	gap-3 icon-btn" id="post-read">
                         <span class="material-icons-outlined icon-style">
-                        fingerprint
+                        bookmarks
                         </span>
                         <div class="text-sm text-style text-style">Read</div>
                     </div>
@@ -37,8 +43,8 @@
                 </div>
                 <div class="flex items-center justify-between mt-4">
                     <img :src="user_avatarURL"  class="bg-yellow-500 rounded-full w-10 h-10 object-cover border">
-                    <div class="flex items-center	justify-between	 bg-gray-50 h-11 w-11/12 border rounded-2xl	 overflow-hidden px-4 ">
-                        <input type="search" class="text-style w-full py-2 pl-4 pr-10 text-sm bg-gray-100 border border-transparent appearance-none rounded-tg placeholder-gray-400 focus:bg-white focus:outline-none focus:border-blue-500 focus:text-gray-900 focus:shadow-outline-blue" style="border-radius: 25px" placeholder="Post a comment..." autocomplete="off"/>
+                    <div class="flex items-center	justify-between	 bg-gray-50 h-11 w-11/12 border rounded-2xl	 overflow-hidden px-4 text-style">
+                        <input type="search" class="w-full py-2 pl-4 pr-10 text-sm bg-gray-100 border border-transparent appearance-none rounded-tg placeholder-gray-400 focus:bg-white focus:outline-none focus:border-blue-500 focus:text-gray-900 focus:shadow-outline-blue" style="border-radius: 25px" placeholder="Post a comment..." autocomplete="off"/>
                     </div>
                 </div>
             </div>
