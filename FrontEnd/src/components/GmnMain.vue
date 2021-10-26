acb19caa2399b18b5512e408472cf9d53b889801
j0rdan-m <jordan.mariani>
2021-10-27 01:05:08 +0200

    fix responsive profile

--- FrontEnd/src/components/GmnMain.vue
+++ FrontEnd/src/components/GmnMain.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="bg-gray-200 font-sans h-screen w-full flex flex-row justify-center items-center" id="main">
+
         <div class="card w-96 mx-auto bg-white  shadow-xl hover:shadow" id="card-user">
+            <div style="height:290px"></div>
             <img class="w-32 mx-auto rounded-full -mt-20 border-8 border-black" id="round-img" :src="user_avatarURL" alt="">
             <div class="text-center mt-2 text-3xl font-medium" id="text">{{ user_firstname }} {{ user_lastname }}</div>
             <div class="text-center mt-2 font-light text-sm" id="text">Groupomania</div>
@@ -12,7 +13,6 @@
             </div>
             <hr class="mt-8">
         </div>
-    </div>
 </template>
 
 <script>
